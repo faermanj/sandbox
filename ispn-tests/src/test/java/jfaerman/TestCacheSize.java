f2a010c8deee1eb6edd11e7f0f7d9ac7e7640f4d
Julio Faerman <jfaerman@gmail.com>
2012-05-19 09:41:49 -0300

    Identing...

--- ispn-tests/src/test/java/jfaerman/TestCacheSize.java
+++ ispn-tests/src/test/java/jfaerman/TestCacheSize.java
@@ -9,28 +9,28 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestCacheSize {
-	int sizeBefore = -1;
-	int sizeAfter = -1;
-	
-	@Listener
-	public class CacheSizeListener {
-		@CacheEntryCreated
-		public void onEntryCreated(CacheEntryCreatedEvent<Object, Object> event){
-			if(event.isPre())
-				sizeBefore = event.getCache().size();
-			else
-				sizeAfter = event.getCache().size();
-		}
-	}
-	
-	@Test
-	public void testOnEntryCreated() {
-		DefaultCacheManager manager = new DefaultCacheManager();
-		Cache<Object, Object> cache = manager.getCache();
-		cache.addListener(new CacheSizeListener());
-		cache.put(new Object(), new Object());
-		assertEquals(sizeBefore + 1, sizeAfter);
+  int sizeBefore = -1;
+  int sizeAfter = -1;
 
-	}
+  @Listener
+  public class CacheSizeListener {
+    @CacheEntryCreated
+    public void onEntryCreated(CacheEntryCreatedEvent<Object, Object> event) {
+      if (event.isPre())
+        sizeBefore = event.getCache().size();
+      else
+        sizeAfter = event.getCache().size();
+    }
+  }
+
+  @Test
+  public void testOnEntryCreated() {
+    DefaultCacheManager manager = new DefaultCacheManager();
+    Cache<Object, Object> cache = manager.getCache();
+    cache.addListener(new CacheSizeListener());
+    cache.put(new Object(), new Object());
+    assertEquals(sizeBefore + 1, sizeAfter);
+
+  }
 
 }
