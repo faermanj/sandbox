902643eab53bdea7c334f5183751da5a74fd8dd2
Julio Faerman <jfaerman@gmail.com>
2012-06-27 18:56:02 -0300

    ft demo

--- ftclient/src/main/java/jfaerman/InfinispanApplet.java
+++ ftclient/src/main/java/jfaerman/InfinispanApplet.java
@@ -1,14 +1,16 @@
 package jfaerman;
 
-import static org.infinispan.eviction.EvictionStrategy.LIRS;
-
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -25,72 +27,88 @@ import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
-import org.infinispan.notifications.Listener;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
-import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
+import org.infinispan.remoting.transport.Address;
 
-public class HelloWorld extends JApplet {
+public class InfinispanApplet extends JApplet {
 	private static final long serialVersionUID = -6223037066828034775L;
 	private static DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 	private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-	private Cache<Object, Object> replCache;
-	private Cache<Object, Object> localCache;
 
 	final JLabel lblStatus = new JLabel("** Hello World **");
 	final JLabel lblTime = new JLabel("");
-	final JLabel lblEntryLocal = new JLabel("local:chave=[nada]");
 	final JLabel lblEntryRepl  = new JLabel("repl:chave=[nada]");
+	final JLabel lblView = new JLabel("[]");
+	private CacheActor actor;
 
-
+	class CacheActor implements Runnable {
+		private BlockingQueue<AbstractMap.SimpleEntry<Object,Object>> queue = new LinkedBlockingQueue<AbstractMap.SimpleEntry<Object,Object>>();
+		private Cache<Object, Object> replCache;
+		EmbeddedCacheManager manager;
+		public void createCache() {
+			manager = new DefaultCacheManager(
+					GlobalConfigurationBuilder.defaultClusteredBuilder()
+							.transport()
+							.defaultTransport()
+							.build(), new ConfigurationBuilder().clustering()
+							.cacheMode(CacheMode.REPL_SYNC).build());
+			replCache = manager.getCache();
+		}
+		
+		public void run() {
+			createCache();
+			while(true){
+				try {
+					AbstractMap.SimpleEntry<Object,Object> entry = queue.take();
+					replCache.put(entry.getKey(), entry.getValue());
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		public void put(String key,String value){
+			try {
+				SimpleEntry<Object, Object> entry = new AbstractMap.SimpleEntry<Object,Object>(key,value);
+				queue.put(entry);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public Object get(String key){
+			return replCache!=null? replCache.get(key) : null;
+		}
+		
+		public String getViewString(){
+			if (manager==null) return "[-]";
+			StringBuilder str = new StringBuilder();
+			List<Address> members = manager.getMembers();
+			for(Address member:members){
+				str.append(member+", ");
+			}
+			return str.toString();
+		}
+	}
+	
 	@Override
 	public void start() {
-		//TODO: Dont do so much in start()
-		createLocalCache();
-		createReplicatedCache();
-		bindCaches();
+		actor = new CacheActor();
+		new Thread(actor).start();
 		executor.scheduleAtFixedRate(new Runnable() {
 			public void run() {
 				updateUI();	
 			}
-		}, 10, 1, TimeUnit.SECONDS);
+		}, 0, 1, TimeUnit.SECONDS);
 	}
 	
 	// Called when this applet is loaded into the browser.
 	public void init() {
 		createUI();
 	}
-
-	private void createReplicatedCache() {
-		EmbeddedCacheManager manager = new DefaultCacheManager(
-				GlobalConfigurationBuilder.defaultClusteredBuilder()
-						.transport()
-						.defaultTransport()
-						.build(), new ConfigurationBuilder().clustering()
-						.cacheMode(CacheMode.REPL_SYNC).build());
-		replCache = manager.getCache();
-	}
-
-	private void createLocalCache() {
-		EmbeddedCacheManager manager = new DefaultCacheManager();
-		manager.defineConfiguration("local-cache", new ConfigurationBuilder()
-				.clustering().cacheMode(CacheMode.LOCAL).build());
-		localCache = manager.getCache("local-cache");
-	}
 	
-	@Listener
-	public class CopyListener{
-		@CacheEntryModified
-		public void onCacheEntryModified(CacheEntryModifiedEvent<String, String> event){
-			if(event.isPre()) return;
-			String key = event.getKey();
-			String value = event.getValue();
-			replCache.put(key,value);
-		}
-	}
-	
-	private void bindCaches(){
-		localCache.addListener(new CopyListener());
-	}
 
 	public void createUI() {
 		// Execute a job on the event-dispatching thread; creating this applet's
@@ -102,16 +120,7 @@ public class HelloWorld extends JApplet {
 					pnl.setLayout(new BoxLayout(pnl, BoxLayout.PAGE_AXIS));
 					pnl.add(lblStatus);
 					pnl.add(lblTime);
-					pnl.add(lblEntryLocal);
-					JButton btnLocal = new JButton("Do The Local Trick");
-					btnLocal.addActionListener(new ActionListener() {
-
-						public void actionPerformed(ActionEvent e) {
-							doTheLocalTrick();
-						}
-					});
-					
-					pnl.add(btnLocal);
+					pnl.add(lblView);
 					
 					pnl.add(lblEntryRepl);
 					JButton btnRepl = new JButton("Do The Repl Trick");
@@ -137,8 +146,8 @@ public class HelloWorld extends JApplet {
 			SwingUtilities.invokeAndWait(new Runnable() {
 				public void run() {
 					lblTime.setText("Current time: " + sdf.format(new Date()));
-					lblEntryLocal.setText("local:chave="+localCache.get("chave"));
-					lblEntryRepl.setText("repl:chave="+replCache.get("chave"));
+					lblEntryRepl.setText("repl:chave="+actor.get("chave"));
+					lblView.setText(actor.getViewString());
 				}
 			});
 		} catch (Exception ex) {
@@ -147,12 +156,8 @@ public class HelloWorld extends JApplet {
 	}
 	
 	public void doTheReplTrick(){
-		replCache.put("chave", "the repl trick done @ "+sdf.format(new Date()));
+		actor.put("chave", "the repl trick done @ "+sdf.format(new Date()));
 		lblStatus.setText("The repl trick is done");
 	}
 	
-	public void doTheLocalTrick(){
-		localCache.put("chave", "the local trick done @ "+sdf.format(new Date()));
-		lblStatus.setText("The local trick is done");
-	}
 }
